2d6558e7f48ce6c92daa327973f8acb13d8724f5
Subbu <svenkat683@gmail.com>
2026-06-14 22:44:06 +0530

    Decode the MP3 stream and play using oto audio library

--- main.go
+++ main.go
@@ -3,51 +3,86 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
+
+	"github.com/ebitengine/oto/v3"
+	"github.com/hajimehoshi/go-mp3"
 )
 
 const url = "https://coderadio-admin-v2.freecodecamp.org/listen/coderadio/radio.mp3"
 
 func main() {
-	client := &http.Client{}
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
+func run() error {
+	transport := &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   10 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ResponseHeaderTimeout: 10 * time.Second,
+	}
+	client := &http.Client{
+		Transport: transport,
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
-		log.Fatal(err)
-		return
+		return err
 	}
 
 	res, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	defer func() { _ = res.Body.Close() }()
 
 	if res.StatusCode < http.StatusOK || res.StatusCode >= http.StatusMultipleChoices {
-		log.Fatalf("Unable to process the stream: %s", res.Status)
+		return fmt.Errorf("unable to process the stream: %s", res.Status)
 	}
 
-	out, err := os.Create("capture.mp3")
+	decoded, err := mp3.NewDecoder(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	written, err := io.Copy(out, res.Body)
+	op := &oto.NewContextOptions{
+		SampleRate:   decoded.SampleRate(),
+		ChannelCount: 2,
+		Format:       oto.FormatSignedInt16LE,
+	}
+	otoCtx, ready, err := oto.NewContext(op)
 	if err != nil {
-		log.Fatal(err)
+		return err
+	}
+	<-ready
+
+	player := otoCtx.NewPlayer(decoded)
+	player.Play()
+
+	ticker := time.NewTicker(100 * time.Millisecond)
+	defer ticker.Stop()
+
+	for player.IsPlaying() {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+		}
 	}
 
-	if err := out.Close(); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Bytes written", written)
+	return nil
 }

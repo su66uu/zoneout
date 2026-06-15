5b03d5c817cd552978cd4e742313b654b7faa871
Subbu <svenkat683@gmail.com>
2026-06-15 11:37:52 +0530

    Add logs

--- main.go
+++ main.go
@@ -38,6 +38,8 @@ func run() error {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
 
+	start := time.Now()
+	log.Printf("connecting to %s", url)
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return err
@@ -47,18 +49,25 @@ func run() error {
 	if err != nil {
 		return err
 	}
+	log.Printf("connected in %s: %s content-type=%q", time.Since(start), res.Status, res.Header.Get("Content-Type"))
 
-	defer func() { _ = res.Body.Close() }()
+	defer func() {
+		log.Println("closing the response body")
+		_ = res.Body.Close()
+	}()
 
 	if res.StatusCode < http.StatusOK || res.StatusCode >= http.StatusMultipleChoices {
 		return fmt.Errorf("unable to process the stream: %s", res.Status)
 	}
 
+	log.Println("starting MP3 decode")
 	decoded, err := mp3.NewDecoder(res.Body)
 	if err != nil {
 		return err
 	}
+	log.Println("MP3 decode is ready")
 
+	log.Println("creating audio player")
 	op := &oto.NewContextOptions{
 		SampleRate:   decoded.SampleRate(),
 		ChannelCount: 2,
@@ -69,20 +78,24 @@ func run() error {
 		return err
 	}
 	<-ready
+	log.Println("audio context is ready")
 
 	player := otoCtx.NewPlayer(decoded)
 	player.Play()
+	log.Println("playback started")
 
-	ticker := time.NewTicker(100 * time.Millisecond)
+	ticker := time.NewTicker(250 * time.Millisecond)
 	defer ticker.Stop()
 
 	for player.IsPlaying() {
 		select {
 		case <-ctx.Done():
+			log.Println("stopping: context cancelled")
 			return ctx.Err()
 		case <-ticker.C:
 		}
 	}
 
+	log.Println("Playback ended normally")
 	return nil
 }
